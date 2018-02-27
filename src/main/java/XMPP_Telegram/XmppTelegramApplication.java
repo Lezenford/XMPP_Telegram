a27683d677259ee19627648806d87509c76486ea
Lezenford <plekhanov_a.v@mail.ru>
2018-02-27 13:41:28 +0300

    Initial commit

---
+++ src/main/java/XMPP_Telegram/XmppTelegramApplication.java
@@ -0,0 +1,12 @@
+package XMPP_Telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmppTelegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmppTelegramApplication.class, args);
+	}
+}
